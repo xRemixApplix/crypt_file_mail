da5501fada2b41e33dd637e8eb35c458444682a0
xRemixApplix <r3m1.appli@gmail.com>
2020-01-16 13:43:42 +0100

    Formatage des informations du fichier .csv d'informations

--- module/conso_file.py
+++ module/conso_file.py
@@ -13,20 +13,50 @@ ENTETE = "id_cp; bd; p; val; eq_kwh; eq_eu"
 
 
 # FONCTIONS
-def creation():
+def creation(file_name):
     """
         Creation du fichier .csv contenant les informations des mesures
         à envoyer par mail.
     """
     list_conso = [ENTETE]
+    date = name = ""
 
-    for data in excel.lecture('../test.xlsx', 'DATA'):
-        ligne = str(data[2]) + ";" + str(excel.convert_date(data[0])) + ";" + str(data[0]) + ";" + str(data[1]) + ";;"
-        list_conso.append(ligne)
+    for data in excel.lecture(file_name, 'DATA'):
+        if name != data[2]:
+            date = excel.convert_date(data[0])
+            name = data[2]
+        else:
+            ligne = str(data[2]) + ";"
+            ligne += str(excel.convert_date(data[0]))+ ";"
+            ligne += format_ecart(excel.convert_date(data[0]), date) + ";"
+            ligne += str(round(data[1], 2))
+            ligne += ";;"
 
-    csv.ecriture("test_conso", list_conso)
+            date = excel.convert_date(data[0])
+
+            list_conso.append(ligne)
+
+    csv.ecriture("module/test_conso", list_conso)
+
+
+def format_ecart(date_d, date_d_1):
+    """
+        Formate l'ecart de temps entre les deux informations.
+    """
+    ecart = date_d-date_d_1
+
+    ecart = str(ecart)
+
+    if ecart == "0:01:00":
+        ecart = "1 minute"
+    elif ecart == "0:10:00":
+        ecart = "10 minutes"
+    elif ecart == "1:00:00":
+        ecart = "1 heure"
+
+    return ecart
 
 
 # AUTO-LANCEMENT
 if __name__ == '__main__':
-    creation()
+    creation('test.xlsx')
